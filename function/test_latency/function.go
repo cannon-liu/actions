c4d8ae72359ca648ca73c167efa26ef8da312bd8
cannon-liu <woliuliwen@163.com>
2022-10-14 19:37:46 +0800

    Add files via upload

---
+++ function/test_latency/function.go
@@ -0,0 +1,24 @@
+package test_latency
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+)
+
+// TestLatency is an HTTP Cloud Function with a request parameter.
+func TestLatency(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Name string `json:"name"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, "Hello, Latency!")
+		return
+	}
+	if d.Name == "" {
+		fmt.Fprint(w, "Hello, Latency!")
+		return
+	}
+	fmt.Fprintf(w, "Hello, %s!\n", html.EscapeString(d.Name))
+}
